fd52b772783fbb4f2bf42f6da100ab81d304dc70
sky19930112 <sky19930112@gmail.com>
2023-11-10 22:38:26 -0600

    get basic fuction idea

---
+++ script.js
@@ -0,0 +1,15 @@
+let timer;
+let isRunning = false;
+
+const startButton = document.getElementById("startButton");
+const resetButton = document.getElementById("resetButton");
+const clock = document.getElementById("timer");
+
+function startTimer(){
+}
+
+startButton.addEventListener("click", function(){
+});
+
+resetButton.addEventListener("click", function(){
+});
